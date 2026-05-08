323c3615b48e5729363cec9d1cb5c9e2eb1f8fb3
Muhamad-khalil <medo.khalil.01.18@gmail.com>
2026-05-08 20:18:03 +0300

    Setting Up Pages and Routes and Building the App Layout

---
+++ src/ui/Heading.jsx
@@ -0,0 +1,32 @@
+import styled, { css } from "styled-components";
+
+const test = css`
+  text-align: center;
+  /* background-color: yellow; */
+`;
+
+const Heading = styled.h1`
+  ${(prop) =>
+    prop.type === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+
+  ${(prop) =>
+    prop.type === "h2" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+
+  ${(prop) =>
+    prop.type === "h3" &&
+    css`
+      font-size: 2rem;
+      font-weight: 500;
+    `}
+  line-height : 1.4;
+`;
+
+export default Heading;
