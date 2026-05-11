9cdaf2d1f6badaf959e0106e31701680d4b0501b
Muhamad-khalil <medo.khalil.01.18@gmail.com>
2026-05-11 21:03:02 +0300

    Mutations: Deleting a Cabin

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabins } from "../../services/apiCabins";
 
 const TableRow = styled.div`
   display: grid;
@@ -41,7 +43,20 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
+
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabins,
+    onSuccess: () => {
+      alert("Cabin Successfully");
+      queryClient.invalidateQueries({
+        queryKey: "cabins",
+      });
+    },
+    onError: (err) => alert(err.message),
+  });
   return (
     <TableRow>
       <Img src={image} />
@@ -49,7 +64,9 @@ function CabinRow({ cabin }) {
       <div>first up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button>Delete</button>
+      <button onClick={() => mutate(id)} disabled={isDeleting}>
+        Delete
+      </button>
     </TableRow>
   );
 }
