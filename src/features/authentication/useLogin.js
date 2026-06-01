9acaa3380dca906ce65a06ca708d82868b86b816
Muhamad-khalil <medo.khalil.01.18@gmail.com>
2026-06-01 20:51:27 +0300

    User Logout and create the user Sign Up

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (data) => {
       queryClient.setQueryData(["user"], data.user);
-      navigate("/");
+      navigate("/dashboard", { replace: true });
     },
 
     onError: (err) => {
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -9,6 +9,21 @@ export async function login({ email, password }) {
   if (error) throw new Error(error.message);
   return data;
 }
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+  return data;
+}
 
 export async function getCurrentUser() {
   const { data: session } = await supabase.auth.getSession();
@@ -16,4 +31,9 @@ export async function getCurrentUser() {
   const { data, error } = await supabase.auth.getUser();
   console.log(data);
   return data?.user;
+}
+
+export async function logout() {
+  let { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
